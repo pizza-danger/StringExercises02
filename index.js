45133dfd73cbe558daa6d74bd6301a9f801cd12f
thereyner <>
2021-06-05 01:33:26 +0000

    Type Conversion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
 
-
+console.log((String(num)).length);
 
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
